2af12bd2756df7d79be4b1ca9751d6bf239d71be
weirukai <15623739350@163.com>
2019-07-03 23:28:26 +0800

    third

--- bottle/pages/Secpages/voice/voice.js
+++ bottle/pages/Secpages/voice/voice.js
@@ -27,47 +27,44 @@ Page({
     title:"心愿瓶——记录你的心灵之声",
     recording:false,//录音开启
     hasrecord: false,//录音结束
-    playing: false,
     recordingtime:"00:00:00",
     recordtime:0,
     playingtime:"00:00:00",
     playtime:0,
-
-
-    playingvoice:true,//palyvoice函数启动之后更新，用来控制stop
-
-
+    playingvoice:false,//palyvoice函数启动之后更新，用来控制stop
+    // tempFilePath
   },//data中的数据都是private的，需要调用对象的对外接口（方法）才能修改，
   //这里提供的修改接口是setData
-  
+
+
 
   startRecord()
   {
-    this.setData(
-      {
+    this.setData({
         recording:true
-      }
-    )
+      } )
     const that=this//对象转换，方便在闭包中访问page中的属性
 //时间开始，开始计时 setinterval
 recordinterval=setInterval(
   function(){
-   
     var recordtime=that.data.recordtime+1//更新时间,data对象里面的东西要用 .
    var recordingtime= formatTime(recordtime) //时间转换
-    that.setData(                                         //刷新视图层和data的中
-                {
+    that.setData(                                         //刷新视图层和data的
+      {
                   recordingtime,
                   recordtime,
-                }
-    )
-     //console.log(recordingtime)
+      })        //console.log(recordingtime)
+   
   },1000)
-  WX:wx.startRecord({
+   wx.startRecord({
     success: function(res) {
       const temppath=res.tempFilePath
-
+      that.setData({
+        tempFilePath:temppath,
+      })
+      //console.log(temppath)
     },
+    
     fail: function(res) {},
     complete: function(res) {
       that.setData({
@@ -77,8 +74,10 @@ recordinterval=setInterval(
       clearInterval(recordinterval)
     },
   })
-  
 },
+
+
+
   stopRecord(){
   wx.stopRecord()
   },//点完之后相当于执行上面的complete
@@ -86,20 +85,95 @@ recordinterval=setInterval(
 
 
 stopvoice(){
-//停止播放录音
+//播放录音
+  clearInterval(playinterval)
+  this.setData({
+   playingtime: formatTime(0),
+    playtime: 0
+  })
+  wx.stopVoice()
 
 },
+
+
+
+
+
 playVoice()
 {
+  const that=this
+ // console.log(1)通过log确认事件响应了
+  //开始播放录音,控制stopvoice可以响应
+  playinterval=setInterval( function()
+  {
+    var playtime=that.data.playtime+1
+    var playingtime=formatTime(playtime)
+    that.setData(
+     { playtime,
+      playingtime,
+     }
+    )
+  },1000)
+  this.setData({
+    playingvoice:true,
+  })
+  wx:wx.playVoice(
+    {
+    filePath: this.data.tempFilePath,
+    duration: 60,
+    //录音播放完毕回调success
+    //
+    //大bug！！！！！手机端显示正常，电脑端异常！！！！
+    //
+    success: function(res) {
+      //console.log(that.data.tempFilePath)
+      clearInterval(playinterval)
+      that.setData(
+        {
+          playtime: 0,
+          playingtime: "00:00:00",
+          playingvoice: false
+        })
+    },
+    fail: function(res) 
+    {
+      //console.log(0)
+    },
+    complete: function(res) {
+      /*clearInterval(playinterval)
+        */
+    },
+  })
   
-  //开始播放录音
 },
 clear()
 {
-//清除录音
+//再次跳转到这个页面
+wx.navigateTo({
+  url: 'voice',
+  success: function(res) {},
+  fail: function(res) {},
+  complete: function(res) {},
+})
 },
 
 
+throwbottle(){
+  //后期动画
+  var that=this
+  wx.saveFile({
+  tempFilePath:that.data.tempFilePath,
+  success(res){
+    const savedfilepath=res.savedFilePath
+    that.setData({
+      savedfilepath
+    })
+  },
+  fail(){
+  console.log("saveerror")
+  }
+  })
+},
 
 
 
